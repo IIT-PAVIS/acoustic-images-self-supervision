2cd99cdd8d3cdeac85fd306c799c0b79d10a68f2
Valentina Sanguineti <valentina.sanguineti@iit.it>
2020-07-07 16:59:51 +0200

    comments

--- script/startAudio.bash
+++ script/startAudio.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-HOME_DIR="/data/vsanguineti/"
-ROOT_DIR="/data/vsanguineti/tfrecords/lists/"
+HOME_DIR="/data/vsanguineti/" #folder where saving tensorboard and checkpoints
+ROOT_DIR="/data/vsanguineti/tfrecords/lists/" # folder where dataset tfrecords are
 
 EXP=(Acoustic12820epochs_ Audio12820epochs_ )
 MODEL=(DualCamHybridNet HearNet)
@@ -14,7 +14,7 @@ for t in "${ST[@]}"
 do
     for ((a = 0 ; a < ${#EXP[@]} ; a++))
     do
-        CUDA_VISIBLE_DEVICES=$i python main.py --mode train --model_1 ${MODEL1[$a]} --model_2 ${MODEL[$a]} --train_file  $ROOT_DIR"/training.txt" --valid_file $ROOT_DIR"/validation.txt" --test_file $ROOT_DIR"/testing.txt"  --exp_name ${EXP[$a]}$t --batch_size 64 --total_length 2 --number_of_crops 1 --sample_length 2 --buffer_size 10 --log_dir $HOME_DIR"/tensorboard/" --checkpoint_dir $HOME_DIR"/checkpoints/" --num_epochs 20 --learning_rate 0.00001 --embedding 1 --temporal_pooling 1 --margin 0.2 &
+        CUDA_VISIBLE_DEVICES=$i python main.py --mode train --model_1 ${MODEL1[$a]} --model_2 ${MODEL[$a]} --train_file  $ROOT_DIR"/training.txt" --valid_file $ROOT_DIR"/validation.txt" --test_file $ROOT_DIR"/testing.txt"  --exp_name ${EXP[$a]}$t --batch_size 64 --total_length 2 --number_of_crops 1 --sample_length 2 --buffer_size 10 --tensorboard $HOME_DIR"/tensorboard/" --checkpoint_dir $HOME_DIR"/checkpoints/" --num_epochs 20 --learning_rate 0.00001 --embedding 1 --temporal_pooling 1 --margin 0.2 &
         #echo "gpu: $i" &
         P[$i]=$!
         #echo "pid: ${P[$i]}"
